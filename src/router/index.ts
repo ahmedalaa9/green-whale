46824eb648f3d43ee57e4f49082ab3e9cf79fe6d
ahmed <ahmedalaa96666@gmail.com>
2026-05-19 17:59:42 +0300

    fix build

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/about', name: 'about', component: () => import('../views/AboutView.vue') },
-    { path: '/services', name: 'services', component: () => import('../views/ServicesView.vue') },
-    { path: '/gallery', name: 'gallery', component: () => import('../views/GalleryView.vue') },
-    { path: '/certificates', name: 'certificates', component: () => import('../views/CertificatesView.vue') },
-    { path: '/contact', name: 'contact', component: () => import('../views/ContactView.vue') },
+    { path: "/", name: "home", component: HomeView },
+    {
+      path: "/about",
+      name: "about",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/services",
+      name: "services",
+      component: () => import("../views/ServicesView.vue"),
+    },
+    {
+      path: "/gallery",
+      name: "gallery",
+      component: () => import("../views/GalleryView.vue"),
+    },
+    {
+      path: "/certificates",
+      name: "certificates",
+      component: () => import("../views/CertificatesView.vue"),
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: () => import("../views/ContactView.vue"),
+    },
   ],
   scrollBehavior(_to, _from, savedPosition) {
-    if (savedPosition) return savedPosition
-    return { top: 0, behavior: 'smooth' }
+    if (savedPosition) return savedPosition;
+    return { top: 0, behavior: "smooth" };
   },
-})
+});
 
-export default router
+export default router;
